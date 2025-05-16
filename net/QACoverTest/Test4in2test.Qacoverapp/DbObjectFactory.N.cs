1a11df0dec09eea786a8abe2e11394cd09106310
Javier <10879637+javiertuya@users.noreply.github.com>
2025-05-16 16:12:05 +0200

    Update deprecated .NET db Provider

--- net/QACoverTest/Test4in2test.Qacoverapp/DbObjectFactory.N.cs
+++ net/QACoverTest/Test4in2test.Qacoverapp/DbObjectFactory.N.cs
@@ -16,7 +16,7 @@ namespace Test4giis.Qacoverapp
                 //return new Microsoft.Data.Sqlite.SqliteConnection(url);
                 return new System.Data.SQLite.SQLiteConnection(url);
             else if (variant.IsSqlServer())
-                return new System.Data.SqlClient.SqlConnection(url);
+                return new Microsoft.Data.SqlClient.SqlConnection(url);
             else
                 throw new QaCoverException("ConnectionFactory.GetNativeConnection: Variant '" + variant.GetSgbdName() + "' not supported");
         }
@@ -28,7 +28,7 @@ namespace Test4giis.Qacoverapp
                 //p1 = new Microsoft.Data.Sqlite.SqliteParameter(name, Microsoft.Data.Sqlite.SqliteType.Integer);
                 p1 = new System.Data.SQLite.SQLiteParameter(name, DbType.Int32);
             else if (variant.IsSqlServer())
-                p1 = new System.Data.SqlClient.SqlParameter(name, SqlDbType.Int);
+                p1 = new Microsoft.Data.SqlClient.SqlParameter(name, SqlDbType.Int);
             else
                 throw new QaCoverException("AppBase.GetNetParam: Variant '" + variant.GetSgbdName() + "' not supported");
             p1.DbType = DbType.Int32; //para que no muestre comillas al mostrar los parametros como string
@@ -42,7 +42,7 @@ namespace Test4giis.Qacoverapp
                 //p1 = new Microsoft.Data.Sqlite.SqliteParameter(name, Microsoft.Data.Sqlite.SqliteType.Text);
                 p1 = new System.Data.SQLite.SQLiteParameter(name, DbType.String);
             else if (variant.IsSqlServer())
-                p1 = new System.Data.SqlClient.SqlParameter(name, SqlDbType.VarChar);
+                p1 = new Microsoft.Data.SqlClient.SqlParameter(name, SqlDbType.VarChar);
             else
                 throw new QaCoverException("AppBase.GetNetParam: Variant '" + variant.GetSgbdName() + "' not supported");
             p1.Value = value;
@@ -55,7 +55,7 @@ namespace Test4giis.Qacoverapp
                 //p1 = new Microsoft.Data.Sqlite.SqliteParameter(name, System.DBNull.Value);
                 p1 = new System.Data.SQLite.SQLiteParameter(name, System.DBNull.Value);
             else if (variant.IsSqlServer())
-                p1 = new System.Data.SqlClient.SqlParameter(name, System.DBNull.Value);
+                p1 = new Microsoft.Data.SqlClient.SqlParameter(name, System.DBNull.Value);
             else
                 throw new QaCoverException("AppBase.GetNetParam: Variant '" + variant.GetSgbdName() + "' not supported");
             return p1;
